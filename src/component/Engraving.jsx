84bf8adf9aeb40bf527bed9140c0976638b50951
moonjh9392 <moonjh9392@gmail.com>
2022-08-06 16:45:42 +0900

    활성화 버튼 이미지 추가

--- src/component/Engraving.jsx
+++ src/component/Engraving.jsx
@@ -1,24 +1,30 @@
 import "../style/Engraving.css";
+import mococo from "../images/mococo.jpg";
 
 export default function engraving(params) {
   let i = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
   return (
-    <div className="Engraving">
-      <div className="Engraving__img"></div>
-      <div className="Engraving__action">
-        <div className="Engraving__action__name"></div>
-        <div className="Engraving__action__box">
-          {i.map((ele) => {
-            return (
-              <div>
-                <div className="Engraving__action__box__square" key={ele} />
-              </div>
-            );
-          })}
+    <>
+      <div>성공 확률</div>
+      <div className="Engraving">
+        <div className="Engraving__img"></div>
+        <div className="Engraving__action">
+          <div className="Engraving__action__name"></div>
+          <div className="Engraving__action__box">
+            {i.map((ele) => {
+              return (
+                <div>
+                  <div className="Engraving__action__box__square" key={ele} />
+                </div>
+              );
+            })}
+          </div>
+          <div className="Engraving__action__result"></div>
         </div>
-        <div className="Engraving__action__result"></div>
+        <div className="Engraving__btn">
+          <img src={mococo} alt="mococo" />
+        </div>
       </div>
-      <div className="Engraving__btn"></div>
-    </div>
+    </>
   );
 }
