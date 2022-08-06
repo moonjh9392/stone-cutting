20d053bc582cabc564e634bbfe515a6b33ab6c31
moonjh9392 <moonjh9392@gmail.com>
2022-08-06 10:34:06 +0900

    각인 박스 수정

--- src/component/Engraving.jsx
+++ src/component/Engraving.jsx
@@ -1,10 +1,23 @@
 import "../style/Engraving.css";
 
 export default function engraving(params) {
+  let i = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
   return (
     <div className="Engraving">
       <div className="Engraving__img"></div>
-      <div className="Engraving__action"></div>
+      <div className="Engraving__action">
+        <div className="Engraving__action__name"></div>
+        <div className="Engraving__action__box">
+          {i.map((ele) => {
+            return (
+              <div>
+                <div className="Engraving__action__box__square" key={ele} />
+              </div>
+            );
+          })}
+        </div>
+        <div className="Engraving__action__result"></div>
+      </div>
       <div className="Engraving__btn"></div>
     </div>
   );
