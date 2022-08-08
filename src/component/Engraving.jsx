8bb2cad647285058c9f240885cd2b88dbd1a2f1d
moonjh9392 <moonjh9392@gmail.com>
2022-08-08 17:44:50 +0900

    stone 변수 생성

--- src/component/Engraving.jsx
+++ src/component/Engraving.jsx
@@ -1,30 +1,54 @@
 import "../style/Engraving.css";
 import mokoko from "../images/mokoko.jpg";
 
-export default function Engraving({ className, index }) {
-  let i = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+export default function Engraving({ className, Firstele, Firstidx, stone, setStone, percentage, setPercentage }) {
   const handleBtnOnclik = (index) => {
-    alert(index);
+    let random = Math.floor(Math.random() * 100);
+    let result = false;
+    if (percentage > random) {
+      result = true;
+    }
+    if (result) {
+      if (percentage > 25) {
+        setPercentage(percentage - 10);
+      }
+    } else {
+      if (percentage < 75) {
+        setPercentage(percentage + 10);
+      }
+    }
+    console.log(result);
+    for (let i of stone[index]) {
+      if (!i.checked) {
+        i["result"] = result;
+        i["checked"] = true;
+        break;
+      }
+    }
+    console.log(stone[index]);
   };
   return (
     <>
-      <div>{index === 0 ? "성공확률" : index === 2 ? "균열확률" : ""}</div>
+      <div>
+        {Firstidx === 0 ? "성공확률 " : Firstidx === 2 ? "균열확률 " : ""}
+        {Firstidx !== 1 ? percentage + "%" : ""}
+      </div>
       <div className={"Engraving " + className}>
         <div className="Engraving__img"></div>
         <div className="Engraving__action">
           <div className="Engraving__action__name"></div>
           <div className="Engraving__action__box">
-            {i.map((ele) => {
+            {Firstele.map((ele, idx) => {
               return (
                 <div>
-                  <div className={"Engraving__action__box__square__" + index + "_" + ele} key={ele} />
+                  <div className={"Engraving__action__box__square__" + Firstidx + "_" + idx} key={idx} />
                 </div>
               );
             })}
           </div>
           <div className="Engraving__action__result"></div>
         </div>
-        <div className="Engraving__btn" onClick={() => handleBtnOnclik(index)}>
+        <div className="Engraving__btn" onClick={() => handleBtnOnclik(Firstidx)}>
           <img src={mokoko} alt="mokoko" />
         </div>
       </div>
--- src/component/MainBox.jsx
+++ src/component/MainBox.jsx
@@ -1,12 +1,26 @@
+import { useState } from "react";
 import "../style/MainBox.css";
 import Engraving from "./Engraving";
 
 export default function MainBox(params) {
-  let i = [0, 1, 2];
+  const [stone, setStone] = useState(Array.from(Array(3), () => new Array(10).fill({ result: "", checked: false })));
+  const [percentage, setPercentage] = useState(75);
+
   return (
     <div className="MainBox">
-      {i.map((ele) => {
-        return <Engraving className={ele === 2 ? "decrease" : "increase"} key={ele} index={ele} />;
+      {stone.map((ele, idx) => {
+        return (
+          <Engraving
+            className={idx === 2 ? "decrease" : "increase"}
+            key={idx}
+            Firstele={ele}
+            Firstidx={idx}
+            stone={stone}
+            setStone={setStone}
+            percentage={percentage}
+            setPercentage={setPercentage}
+          />
+        );
       })}
     </div>
   );
